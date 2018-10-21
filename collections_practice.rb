eee5c68f981562e83137b2e9a29291d08947a3d6
Meghan Campbell <mlc03e@gmail.com>
2018-10-21 17:46:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
-# your code goes here
+def begins_with_r(array)
+  if array.all? { |word| word.start_with?("r") }
+    return true
+else
+  return false
+  end
+end
+
+def contain_a(array)
+   array.select { |word| word.include?("a") }
+
+end
+
+def first_wa(array)
+  array.select { |word| word.start_with?("wa") }.first
+end
+
+def remove_non_strings(array)
+  array.keep_if { |obj| (obj.is_a? String) }
+end
+
+def count_elements(array)
+  array.select { |name| name.count(name == name) }
+end
+
+def merge_data(array)
+  
+end
+
+def find_cool(array)
+  
+end
+
+def organize_schools(array)
+  
+end
+  
